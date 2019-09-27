aea3a97112cc4fe93ebb957481c9d5b0b7695649
minu27 <55374626+minu27@users.noreply.github.com>
2019-09-27 13:07:19 -0500

    Cal.java
    This is the class which has all the methods that are called from the apples class.

---
+++ Cal.java
@@ -0,0 +1,26 @@
+public class Cal {
+	public int add(int x, int y)
+	{
+		int z;
+		z=x+y;
+		return z;
+	}
+	public int sub(int x, int y)
+	{
+		int z;
+		z=x-y;
+		return z;
+	}
+	public int mul(int x, int y)
+	{
+		int z;
+		z=x*y;
+		return z;
+	}
+	public int div(int x, int y)
+	{
+		int z;
+		z=x/y;
+		return z;
+	}
+}
